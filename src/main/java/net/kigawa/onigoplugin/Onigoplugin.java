5df3b425249398c45b618761ab0b2f341f9f10e6
kigawa <kigawa.8390@gmail.com>
2021-06-14 16:30:56 +0900

    Initial commit

---
+++ src/main/java/net/kigawa/onigoplugin/Onigoplugin.java
@@ -0,0 +1,17 @@
+package net.kigawa.onigoplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Onigoplugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
