e1ac4f044fc950a779369153fd41c3d6aa13cd17
kigawa <kigawa.8390@gmail.com>
2021-07-04 23:03:46 +0900

    update README.md

--- README.md
+++ README.md
@@ -16,3 +16,6 @@
 # Author
 * kigawa
     * kigawa.8390@gmail.com
+  
+# ToDo
+* https://trello.com/b/N3F9Bb7K/%E3%83%97%E3%83%A9%E3%82%B0%E3%82%A4%E3%83%B3
---
+++ src/main/java/net/kigawa/onigoplugin/command/policeandthief/create/PAndT.java
@@ -0,0 +1,38 @@
+package net.kigawa.onigoplugin.command.policeandthief.create;
+
+import net.kigawa.onigoplugin.OnigoPlugin;
+import net.kigawa.util.plugin.KigawaPlugin;
+import net.kigawa.util.plugin.command.MainCommand;
+import net.kigawa.util.plugin.command.SubCommand;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
+
+public class PAndT extends MainCommand {
+    OnigoPlugin plugin;
+    public PAndT(OnigoPlugin onigoPlugin) {
+        super(onigoPlugin);
+        plugin=onigoPlugin;
+    }
+
+    @Override
+    public String getCommandStr() {
+        return "pandt";
+    }
+
+    @Override
+    public boolean onAlways(CommandSender commandSender, Command command, String s, String[] strings) {
+        return false;
+    }
+
+    @Override
+    public boolean onNotFound(CommandSender commandSender, Command command, String s, String[] strings) {
+        return false;
+    }
+
+    @Override
+    public List<SubCommand> getCommandList() {
+        return null;
+    }
+}
